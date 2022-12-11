0c5327819c3916c45eef8a6f58b8c2e2e6499d69
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2022-12-11 21:37:54 +0700

    feat: add solution for day 7 part 2

--- src/bin/day7/main.rs
+++ src/bin/day7/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     let result = solution("./input/day7.txt");
     println!("result: {result}");
+    let result_part_2 = solution_part_2("./input/day7.txt");
+    println!("result: {result_part_2}");
 }
 
 fn solution(filename: &str) -> usize {
@@ -44,7 +46,84 @@ fn solution(filename: &str) -> usize {
         }
     }
 
+    while !stack.is_empty() {
+        let curr_dir_size = stack.pop().unwrap();
+
+        if curr_dir_size <= 100_000 {
+            total += curr_dir_size;
+        }
+
+        let last = stack.last_mut();
+        match last {
+            Some(val) => *val += curr_dir_size,
+            None => break,
+        }
+    }
+
     total
+}
+
+fn solution_part_2(filename: &str) -> usize {
+    // similar to part 1, but we don't go with the total
+    // instead we will take max size that doesn't exceed the limit
+
+    let binding = std::fs::read_to_string(filename).unwrap();
+    let output = binding.lines();
+
+    let mut stack = vec![0];
+    let mut all_dirs = vec![];
+    let mut max = 0;
+    // let mut total = 0;
+    for line in output {
+        // ignore root, ls 
+        if line == "$ cd /" || line == "$ ls" {
+            continue;
+        }
+
+        if line.starts_with("$ cd") {
+            let cmd = line.split_whitespace().collect::<Vec<_>>();
+            if cmd[2] == ".." {
+                let curr_dir_size = stack.pop().unwrap();
+                all_dirs.push(curr_dir_size);
+                // if curr_dir_size > max && curr_dir_size <= 30000000 {
+                //     max = curr_dir_size;
+                // }
+                // if curr_dir_size <= 100_000 {
+                //     total += curr_dir_size;
+                // }
+
+                *stack.last_mut().unwrap() += curr_dir_size;
+            } else {
+                stack.push(0);
+            }
+        } else {
+            let file_desc = line.split_whitespace().collect::<Vec<_>>();
+            let size = file_desc[0].parse::<usize>();
+            match size {
+                Ok(val) => *stack.last_mut().unwrap() += val,
+                _ => println!("ignore"),
+            }
+        }
+    }
+
+    while !stack.is_empty() {
+        let curr_dir_size = stack.pop().unwrap();
+
+        all_dirs.push(curr_dir_size);
+
+        let last = stack.last_mut();
+        match last {
+            Some(val) => *val += curr_dir_size,
+            None => break,
+        }
+    }
+
+    let total_space = 70000000;
+    let space_required = 30000000;
+    let free_space = total_space - all_dirs.last().unwrap();
+    let space_to_delete = space_required - free_space;
+
+    all_dirs.into_iter().filter(|dir| *dir >= space_to_delete).min().unwrap()
 }
 
 #[cfg(test)]
@@ -55,6 +134,12 @@ mod tests {
     fn it_works() {
         let result = solution("./input/day7.test.txt");
         assert_eq!(result, 95437);
+    }
+
+    #[test]
+    fn it_works_part_2() {
+        let result = solution_part_2("./input/day7.test.txt");
+        assert_eq!(result, 24933642);
     }
 }
 
