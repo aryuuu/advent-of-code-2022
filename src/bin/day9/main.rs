1638ca89798f34c78bc59b72dd55c3294dd3099f
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2022-12-12 22:11:08 +0700

    feat: add solution for day 9 part 1

---
+++ src/bin/day9/main.rs
@@ -0,0 +1,109 @@
+use std::collections::HashSet;
+
+fn main() {
+    let result = solution("./input/day9.txt");
+    println!("result: {result}");
+}
+
+fn solution(filename: &str) -> usize {
+    let binding = std::fs::read_to_string(filename).unwrap();
+    let motions = binding.lines().map(|line| {
+        let split = line.split_whitespace().collect::<Vec<_>>();
+        let step = split[1].parse::<usize>().unwrap();
+        (split[0], step)
+    });
+
+    let mut visited = HashSet::<String>::new();
+    visited.insert("0-0".to_string());
+
+    let mut head = Point::default();
+    let mut tail = Point::default();
+
+    for motion in motions {
+        for i in 0..(motion.1) {
+            head.move_dir(motion.0);
+            if tail.is_touching(&head) {
+                continue;
+            }
+
+            tail.chase(&head);
+            visited.insert(format!("{}-{}", tail.x, tail.y));
+        }
+    }
+
+    visited.len()
+}
+
+#[derive(Debug, Default)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    fn chase(&mut self, other: &Point) {
+        // this method assumes self and other does not touch and will be touching again in one
+        // movement
+        if self.x < other.x {
+            self.move_right();
+        }
+        if self.x > other.x {
+            self.move_left();
+        }
+        if self.y < other.y {
+            self.move_up();
+        }
+        if self.y > other.y {
+            self.move_down();
+        }
+    }
+
+    fn is_touching(&self, other: &Point) -> bool {
+        (self.x - other.x).abs() <= 1 && (self.y - other.y).abs() <= 1
+    }
+
+    fn move_dir(&mut self, direction: &str) {
+        match direction {
+            "U" => {
+                self.move_up();
+            }
+            "D" => {
+                self.move_down();
+            }
+            "L" => {
+                self.move_left();
+            }
+            "R" => {
+                self.move_right();
+            }
+            _ => println!("wha"),
+        }
+    }
+
+    fn move_down(&mut self) {
+        self.y -= 1;
+    }
+
+    fn move_up(&mut self) {
+        self.y += 1;
+    }
+
+    fn move_left(&mut self) {
+        self.x -= 1;
+    }
+
+    fn move_right(&mut self) {
+        self.x += 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = solution("./input/day9.test.txt");
+        assert_eq!(result, 13);
+    }
+}
