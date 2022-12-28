7c3f0357ecb7f68557c6c92d10ca223b7d2cf805
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2022-12-28 17:49:20 +0700

    feat: add solution for day 13

---
+++ src/bin/day13/main.rs
@@ -0,0 +1,118 @@
+fn main() {
+    let result = solution("./input/day13.txt");
+    println!("result: {result}");
+}
+
+fn solution(filename: &str) -> usize {
+    let pairs = parse(filename);
+
+    pairs
+        .iter()
+        .enumerate()
+        .filter(|(_, pair)| pair[0] < pair[1])
+        .map(|(idx, _)| idx + 1)
+        .sum()
+}
+
+fn parse(filename: &str) -> Vec<[Packet; 2]> {
+    let input = std::fs::read_to_string(filename).unwrap();
+
+    input
+        .split("\n\n")
+        .map(|pair| {
+            let lines = pair.lines().collect::<Vec<_>>();
+            let left = lines[0];
+            let right = lines[1];
+
+            [parse_packet(left), parse_packet(right)]
+        })
+        .collect()
+}
+
+fn parse_packet(s: &str) -> Packet {
+    let chars: Vec<_> = s.chars().collect();
+    let (packet, _) = parse_list(&chars);
+    packet
+}
+
+fn parse_list(list: &[char]) -> (Packet, &[char]) {
+    let mut list = &list[1..];
+    let mut packets = vec![];
+
+    loop {
+        match list[0] {
+            ']' => break,
+            ',' => list = &list[1..],
+            '[' => {
+                let (packet, rest) = parse_list(list);
+                packets.push(packet);
+                list = rest;
+            }
+            _ => {
+                let (n, rest) = parse_num(list);
+                packets.push(n);
+                list = rest;
+            }
+        }
+    }
+
+    (Packet::List(packets), &list[1..])
+}
+
+fn parse_num(list: &[char]) -> (Packet, &[char]) {
+    let mut i = 0;
+    while i < list.len() && list[i].is_ascii_digit() {
+        i += 1;
+    }
+
+    let mut num = 0;
+    let mut offset = 1;
+    for c in list[..i].iter().rev() {
+        num += (*c as u8 - b'0') * offset;
+        offset *= 10;
+    }
+
+    (Packet::Num(num), &list[i..])
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum Packet {
+    Num(u8),
+    List(Vec<Packet>),
+}
+
+impl Ord for Packet {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match (self, other) {
+            (Self::List(a), Self::List(b)) => a.cmp(b),
+            (Self::List(a), Self::Num(b)) => a.cmp(&vec![Self::Num(*b)]),
+            (Self::Num(a), Self::List(b)) => vec![Self::Num(*a)].cmp(&b),
+            (Self::Num(a), Self::Num(b)) => a.cmp(b),
+        }
+    }
+}
+
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        // let lisa = vec![11, 22, 33, 44];
+        // let result: usize = lisa
+        //     .iter()
+        //     .enumerate()
+        //     .filter(|(idx, val)| **val < 30)
+        //     .map(|(idx, _)| idx + 1)
+        //     .sum();
+        // println!("result: {result}");
+        let result = solution("./input/day13.test.txt");
+        assert_eq!(result, 13);
+    }
+}
